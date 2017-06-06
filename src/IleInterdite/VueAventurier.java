6080361c20ac2753445fea7dc70ad297452b6426
denisu <denisu@pc-dg-039-11>
2017-06-06 11:03:35 +0200

    test

--- src/IleInterdite/VueAventurier.java
+++ src/IleInterdite/VueAventurier.java
@@ -19,9 +19,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VueAventurier implements ActionListener {
-    
+
     private static Controleur leControleur = new Controleur();
-    
+
     private final JPanel panelBoutons;
     private final JPanel panelCentre;
     private final JPanel panelCentrePosition;
@@ -34,18 +34,18 @@ public class VueAventurier implements ActionListener {
     private final JButton btnAutreAction;
     private final JButton btnTerminerTour;
     private final JTextField champCommande;
-    
+
     private final JLabel textePosition;
-    
+
     public VueAventurier(Aventurier unAventurier) {
-        
+
         this.window = new JFrame();
         window.setSize(700, 700);
-        
+
         window.setTitle(unAventurier.getNomJoueur());
         mainPanel = new JPanel(new BorderLayout());
         this.window.add(mainPanel);
-        
+
         mainPanel.setBackground(new Color(230, 230, 230));
         mainPanel.setBorder(BorderFactory.createLineBorder(unAventurier.getPion().getCouleur(), 2));
 
@@ -60,44 +60,49 @@ public class VueAventurier implements ActionListener {
         // CENTRE : 1 ligne pour position courante
         this.panelCentre = new JPanel(new GridLayout(2, 1));
         mainPanel.add(this.panelCentre, BorderLayout.CENTER);
-        
+
         this.panelCentreCarte = new JPanel(new GridLayout(6, 6));
-        panelCentre.add(this.panelCentreCarte, BorderLayout.NORTH);
-        
+        panelCentre.add(this.panelCentreCarte, BorderLayout.CENTER);
+
         this.panelCentrePosition = new JPanel(new GridLayout(2, 1));
         this.panelCentrePosition.setOpaque(false);
         this.panelCentrePosition.setBorder(new MatteBorder(0, 0, 2, 0, unAventurier.getPion().getCouleur()));
         panelCentre.add(this.panelCentrePosition, BorderLayout.SOUTH);
-        
+
         //Grille
         JPanel grille = new JPanel(new GridLayout(6, 6));
-        panelCentreCarte.add(grille);
-        System.out.println(leControleur.getLaGrille().getTuile(0, 3).getNom());
-        int i = 0;
+        //panelCentreCarte.add(grille);
+        // System.out.println(leControleur.getLaGrille().getTuile(0, 3).getNom());
+        int i = 1;
         
+        while (i <= 36) {
+
             for (int l = 0; l <= 5; l++) {
                 for (int c = 0; c <= 5; c++) {
-                    while (i< 36) {
-                    if ((((( l== 0) || (l==5) ) && ( c== 0 || c== 1 || c== 4 || c==5  )) || (((l==1) || (l==4)) && ((c==0) || (c==5))))) {
+
+                    // if (((((l == 0) || (l == 5)) && ((c == 0) || (c == 1) || (c == 4) || (c == 5))) || (((l == 1) || (l == 4)) && ((c == 0) || (c == 5))))) {
+                        if (leControleur.getLaGrille().getTuile(l, c) == null) {
                         JButton tuileVide = new JButton("Vide");
-                        tuileVide.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));                        
+                        tuileVide.setBackground(Color.BLACK);
+
+                        tuileVide.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));
                         panelCentreCarte.add(tuileVide);
                         i++;
-                    } else {
-                        if (leControleur.getLaGrille().getTuile(l, c).getEtat() == Utils.EtatTuile.ASSECHEE) {
-                            JButton tuile = new JButton(leControleur.getLaGrille().getTuile(l, c).getNom() + "[" + l + "," + c + "]");
-                            tuile.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));                            
-                            panelCentreCarte.add(tuile);
-                            i++ ;
-                        } else if (leControleur.getLaGrille().getTuile(l, c).getEtat() == Utils.EtatTuile.INONDEE) {
-                            JButton tuile = new JButton(leControleur.getLaGrille().getTuile(l, c).getNom() + "[" + l + "," + c + "]");
-                            tuile.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));                            
-                            panelCentreCarte.add(tuile);
-                            tuile.setBackground(Color.BLUE);
-                            i++ ;
-                        }
+                    System.out.println("Ligne:"+l+"|"+"Colonne:"+c);
+                            
+                    } else if (leControleur.getLaGrille().getTuile(l, c).getEtat() == Utils.EtatTuile.ASSECHEE) {
+                        JButton tuile = new JButton(leControleur.getLaGrille().getTuile(l, c).getNom() + "[" + l + "," + c + "]");
+                        tuile.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));
+                        panelCentreCarte.add(tuile);
+                        i++;
+                    } else if (leControleur.getLaGrille().getTuile(l, c).getEtat() == Utils.EtatTuile.INONDEE) {
+                        JButton tuile = new JButton(leControleur.getLaGrille().getTuile(l, c).getNom() + "[" + l + "," + c + "]");
+                        tuile.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));
+                        panelCentreCarte.add(tuile);
+                        tuile.setBackground(Color.GRAY);
+                        i++;
                     }
-                    
+
                 }
             }
         }
@@ -118,45 +123,45 @@ public class VueAventurier implements ActionListener {
         this.panelBoutons = new JPanel(new GridLayout(2, 2));
         this.panelBoutons.setOpaque(false);
         mainPanel.add(this.panelBoutons, BorderLayout.SOUTH);
-        
+
         this.btnAller = new JButton("Aller");
         this.btnAssecher = new JButton("Assecher");
         this.btnAutreAction = new JButton("AutreAction");
         this.btnTerminerTour = new JButton("Terminer Tour");
-        
+
         this.panelBoutons.add(btnAller);
         this.panelBoutons.add(btnAssecher);
         this.panelBoutons.add(btnAutreAction);
         this.panelBoutons.add(btnTerminerTour);
-        
+
         this.btnAller.addActionListener(this);
         this.btnTerminerTour.addActionListener(this);
-        
+
         this.window.setVisible(true);
         mainPanel.repaint();
-        
+
     }
-    
+
     public JButton getBtnAutreAction() {
         return btnAutreAction;
     }
-    
+
     public void setChampCommande(String pos) {
         this.champCommande.setText(pos);
     }
-    
+
     public JButton getBtnAller() {
         return btnAller;
     }
-    
+
     public JButton getBtnAssecher() {
         return btnAssecher;
     }
-    
+
     public JButton getBtnTerminerTour() {
         return btnTerminerTour;
     }
-    
+
     public static void main(String[] args) {
         // Instanciation de la fenêtre 
         //VueAventurier vueAventurier = new VueAventurier ("Manon", "Explorateur",Pion.ROUGE.getCouleur() );
@@ -167,9 +172,9 @@ public class VueAventurier implements ActionListener {
                 leControleur.finDuTour();
             }
         }
-        
+
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btnAller) {
